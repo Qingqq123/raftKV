6e6c30e76770fb6f991207eaad46486dbbd97013
Qingqq123 <115354590+Qingqq123@users.noreply.github.com>
2023-08-12 16:10:29 +0800

    Update dict.go

--- datastruct/dict/dict.go
+++ datastruct/dict/dict.go
@@ -1,9 +1,7 @@
 package dict
 
-// Consumer is used to traversal dict, if it returns false the traversal will be break
 type Consumer func(key string, val interface{}) bool
 
-// Dict is interface of a key-value data structure
 type Dict interface {
 	Get(key string) (val interface{}, exists bool)
 	Len() int
